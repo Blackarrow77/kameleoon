44fd3b769528910074193f043cf5f19bed263b58
Guillaume <guillaume.grandjean@epfl.ch>
2019-07-31 11:39:04 +0200

    Backend with Java + Firebase for adding quote

--- src/main/java/io/vertx/starter/MainVerticle.java
+++ src/main/java/io/vertx/starter/MainVerticle.java
@@ -1,5 +1,16 @@
 package io.vertx.starter;
 
+import com.google.api.core.ApiFuture;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.WriteResult;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseToken;
+import com.google.firebase.cloud.FirestoreClient;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.http.HttpMethod;
 import io.vertx.core.http.HttpServer;
@@ -8,19 +19,66 @@ import io.vertx.ext.web.Route;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.BodyHandler;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.rmi.server.UID;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
 import java.util.regex.Matcher;
 
 public class MainVerticle extends AbstractVerticle {
 
   @Override
-  public void start() {
+  public void start() throws IOException {
+
+    FileInputStream serviceAccount = new FileInputStream("kameleoon-firebase-adminsdk-4zr2m-f2951b9096.json");
+    FirebaseOptions options = new FirebaseOptions.Builder()
+      .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+      .setDatabaseUrl("https://kameleoon.firebaseio.com")
+      .build();
+    FirebaseApp.initializeApp(options);
+
+    Firestore db = FirestoreClient.getFirestore();
+
+
     HttpServer server = vertx.createHttpServer();
     Router router = Router.router(vertx);
     router.route().handler(BodyHandler.create());
     Route app = router.route(HttpMethod.POST, "/request");
     app.handler(routingContext -> {
       HttpServerResponse response = routingContext.response();
-      response.end("Test");
+      //Get values
+      String idToken = routingContext.getBodyAsJson().getString("idToken");
+      String title = routingContext.getBodyAsJson().getString("title");
+      String uid = null;
+      String name = null;
+      String id = UUID.randomUUID().toString();
+      try {
+        FirebaseToken decodedToken = FirebaseAuth.getInstance().verifyIdToken(idToken);
+        uid  = decodedToken.getUid();
+        name = FirebaseAuth.getInstance().getUser(uid).getDisplayName();
+      } catch (FirebaseAuthException e) {
+        e.printStackTrace();
+      }
+      DocumentReference docRef = db.collection(uid).document(id);
+      Map<String, Object> data = new HashMap<>();
+      data.put("title", title);
+      data.put("id", id);
+      data.put("author", name);
+      data.put("score", 0);
+      //asynchronously write data
+      ApiFuture<WriteResult> result = docRef.set(data);
+      try {
+        System.out.println("Update time : " + result.get().getUpdateTime());
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      } catch (ExecutionException e) {
+        e.printStackTrace();
+      }
+      response.end("true");
     });
 
     Route home = router.route("/");
